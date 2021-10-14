5c1d537cc1e95b224479b5c5848cef41b6d7f5e3
hojunin <dlsghwns@naver.com>
2021-10-14 12:11:32 +0900

    모듈 수정

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+// https://blog.ull.im/engineering/2018/12/23/how-to-create-and-publish-npm-module-in-typescript.html
+import SliderPicker from './Draggable';
+import NaviModal from './Draggables';
+
+export {SliderPicker, NaviModal};
