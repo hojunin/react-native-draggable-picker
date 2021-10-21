36619686fb7ef6048ac4f184b0bc11c5a803e39c
hojunin <dlsghwns@naver.com>
2021-10-21 22:02:43 +0900

    .d.ts 추가

--- src/Draggables/Draggables.tsx
+++ src/Draggables/Draggables.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {View, Text, Button} from 'react-native';
 import styles from './Draggables.style';
 import Draggable from '../Draggable';
